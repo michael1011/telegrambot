3d840fcbc5d69fc2a4ae0ce38124a6143ee98365
Michael <michael101101@me.com>
2016-07-20 10:30:02 +0200

    test

--- src/at/michael1011/telegrambot/Main.java
+++ src/at/michael1011/telegrambot/Main.java
@@ -3,7 +3,7 @@ package at.michael1011.telegrambot;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("test");
     }
 
 }
